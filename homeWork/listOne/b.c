7479b8f66a6e714582640fd4695cbd87d4813e9d
diegocorrea <diegocorrea.cc@gmail.com>
2016-08-21 20:53:20 -0300

    start home work

---
+++ homeWork/listOne/b.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+const long int MAX = 1000000;
+int inputLength, vector;
+
+void readInput();
+
+int main(){
+
+  readInput();
+
+  return 0;
+}
+
+
+void readInput(){
+  int i;
+
+  scanf("%d", &inputLength);
+
+  for(i < 0; i < inputLength; i++){
+    scanf("%d", &vector);
+  }
+}
