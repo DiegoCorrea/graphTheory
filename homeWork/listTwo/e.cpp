acc033b2d47fa199035452772617c5ba2f11709a
diegocorrea <diegocorrea.cc@gmail.com>
2016-10-03 21:14:05 -0300

    pre finish E

--- homeWork/listTwo/e.cpp
+++ homeWork/listTwo/e.cpp
@@ -38,12 +38,16 @@ class Edge{
 
 class Graph {
   int numberOfVertex;
+  int finalResult;
 
   public:
     vector<Edge> edges;
+    vector<Edge> tree;
+    vector <int> bestestResult;
 
     Graph(int numberOfVertex){
       this->numberOfVertex = numberOfVertex;
+      edges.clear();
     }
 
     void addEdge(int origin, int master, int distance) {
@@ -51,6 +55,27 @@ class Graph {
       edges.push_back(edge);
     }
 
+    void addNewWayValor(int newBest){
+      bestestResult.push_back(newBest);
+      sort(bestestResult.begin(), bestestResult.end());
+    }
+
+    int getNumberOfVertex(){
+      return this->numberOfVertex;
+    }
+    
+    int getNumberOfEdgesOnTree(){
+      return tree.size();
+    }
+
+    int getResult(){
+      return finalResult;
+    }
+
+    int getSecondResult(){
+      return bestestResult[0];
+    }
+    
     int search(int subset[], int i){
       if(subset[i] == ALONE)
         return i;
@@ -64,14 +89,21 @@ class Graph {
     }
 
     void kruskal() {
-      vector<Edge> tree;
+      tree.clear();
+      int size_subset;
       int size_edges = edges.size();
 
       sort(edges.begin(), edges.end());
 
-      int *subset = new int[size_edges+2];
+      if(size_edges > numberOfVertex){
+        size_subset = size_edges+1;
+      } else {
+        size_subset = numberOfVertex+1;
+      }
 
-      memset(subset, ALONE, sizeof(int) * (size_edges+2));
+      int *subset = new int[size_subset];
+
+      memset(subset, ALONE, sizeof(int) * (size_subset));
 
       for(int i = 0; i < size_edges; i++) {
         int origin = search(subset, edges[i].getOrigin());
@@ -82,25 +114,37 @@ class Graph {
           junction(subset, origin, master);
         }
       }
-
       int size_tree = tree.size();
       int result = 0;
 
       for(int i = 0; i < size_tree; i++) {
         result += tree[i].getDistance();
       }
-      printf("%d\n", result);
+      finalResult = result;
+    }
+
+    bool haveMST(){
+      if(this->getNumberOfEdgesOnTree() != this->getNumberOfVertex()-1){
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    void removeEdge(int edgeToRemove){
+      edges.erase(edges.begin() + edgeToRemove);
     }
 };
 
 int main(){
   int n, m, input;
   int origin, master, distance;
+  int ways;
 
   scanf("%d", &input);
 
   for(int test = 1; test <= input; test++){
-    scanf("%d %d", &n, &m)
+    scanf("%d %d", &n, &m);
 
     Graph g(n);
 
@@ -108,9 +152,43 @@ int main(){
       scanf("%d %d %d", &origin, &master, &distance);
       g.addEdge(origin, master, distance);
     }
+    g.kruskal();
     
+    if(!g.haveMST()){
+      ways = 0;
+    } else{
+      ways = 1;
+      for(int edgeTree = 0; edgeTree < g.getNumberOfEdgesOnTree();edgeTree++){
+        Graph tryNewWay = g;
+        
+        tryNewWay.removeEdge(edgeTree);
+        tryNewWay.kruskal();
+        if(tryNewWay.haveMST()){
+          g.addNewWayValor(tryNewWay.getResult());
+          ways = 2;
+        }
+      }
+    }
+  
     printf("Case #%d : ", test);
-    //g.kruskal();
+
+    switch(ways){
+      case 0:
+        printf("No way\n");
+      break;
+
+      case 1:
+        printf("No second way\n");
+      break;
+
+      case 2:
+        printf("%d\n", g.getSecondResult());
+      break;
+
+      default:
+        printf("Opção 3\n");
+      break;
+    }
   }
   return 0;
 }
